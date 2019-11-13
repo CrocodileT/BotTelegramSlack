19a2b0e35fccddfacf45c61aaeae4b3be348b849
Grigoriy Mikheev <grigoriy-mikheev@mail.ru>
2019-11-13 04:59:11 +0300

    added button for telegram

--- app/Main.hs
+++ app/Main.hs
@@ -17,11 +17,67 @@ import qualified Data.ByteString.Char8 as B
 import Data.Aeson.Types 
 import qualified Data.HashMap.Strict as HM
 import qualified Network.HTTP.Client as CL
+import qualified Data.ByteString.Lazy as LB
 
 import Lib
 import Config
 import ParseJSON
 import BotCommand
+
+{-key1 :: KeyButton
+key1 = object [
+  "text" .= "1"]
+
+key2 :: KeyButton
+key2 = object [
+  "text" .= "2"]
+
+key3 :: KeyButton
+key3 = object [
+  "text" .= "3"]
+
+key4 :: KeyButton
+key4 = object [
+  "text" .= "4"]
+
+key5 :: KeyButton
+key5 = object [
+  "text" .= "5"]
+
+replyKeyboardMarkup :: ReplyKey
+replyKeyboardMarkup = object [
+  "keyboard" .= [key1, key2, key3, key4, key5]]-}
+
+key1 :: KeyButton
+key1 = KeyButton "1"
+
+key2 :: KeyButton
+key2 = KeyButton "2"
+
+key3 :: KeyButton
+key3 = KeyButton "3"
+
+key4 :: KeyButton
+key4 = KeyButton "4"
+
+key5 :: KeyButton
+key5 = KeyButton "5"
+
+buttons :: ReplyKey
+buttons = ReplyKey [[key1 , key2 , key3 ,key4 , key5]]
+
+data KeyButton = KeyButton {
+  text :: T.Text } deriving (Generic, Show)
+
+data ReplyKey = ReplyKey {
+  keyboard :: [[KeyButton]] } deriving (Generic, Show)
+
+instance ToJSON KeyButton
+instance FromJSON KeyButton
+
+instance ToJSON ReplyKey
+instance FromJSON ReplyKey
+
 
 --proxyHttpConfig = defaultHttpConfig { httpConfigProxy = Just (CL.Proxy (B.pack $ "200.111.182.6") 443)}
 proxyHttpConfig = defaultHttpConfig { httpConfigProxy = Just (CL.Proxy (B.pack $ "141.125.82.106") 80)}
@@ -32,16 +88,16 @@ fromResultToList (Success a) = a
 fromResultToList (Error e) = [(e,0,0)]
 
 answerTelegram :: (HttpMethod method, HttpBodyAllowed (AllowsBody method) (ProvidesBody NoReqBody)) => 
-  method -> [String] -> Req (JsonResponse Value)
+  method -> [B.ByteString] -> Req (JsonResponse Value)
 answerTelegram method args = do
-  let urlHttps = B.pack $ "https://api.telegram.org/bot" ++ tokenTelegram ++ (foldr (++) "" args) 
+  let urlHttps = (B.pack $ ("https://api.telegram.org/bot" ++ tokenTelegram)) <> (foldr (<>) "" args) 
       (url, options) = fromJust $ parseUrlHttps urlHttps
   req method url NoReqBody jsonResponse options
 
-receiveTelegram :: [String] -> Req (JsonResponse Value)
+receiveTelegram :: [B.ByteString] -> Req (JsonResponse Value)
 receiveTelegram args = answerTelegram GET args
 
-sendTelegram :: [String] -> Integer -> Req (JsonResponse Value)
+sendTelegram :: [B.ByteString] -> Integer -> Req (JsonResponse Value)
 sendTelegram args 1 = answerTelegram POST args
 sendTelegram args countRepeat = do
   answerTelegram POST args
@@ -55,7 +111,9 @@ updateUser :: Integer -> Integer -> InfoUsers -> Req a
 updateUser update_id chat_id users = do
   let newRepeat = countRepeat chat_id (fst users)
   let newUsers = (updateUsers chat_id (fst users), snd users)
-  sendTelegram ["/sendMessage?chat_id=", show chat_id, "&text=", messageRepeat, (show newRepeat)] defaultRepeat
+  --let buttons = ReplyKey { KeyButton "1", KeyButton "2", KeyButton "3", KeyButton "4", KeyButton "5" }
+  let args = (B.pack <$> ["/sendMessage?chat_id=", show chat_id, "&text=", messageRepeat, (show newRepeat), "&reply_markup="]) <> [LB.toStrict $ encode $ buttons]
+  sendTelegram args 1
   loop (update_id + 1) newUsers
 
 checkButton :: Integer -> Integer -> String -> InfoUsers -> Req a
@@ -67,21 +125,23 @@ checkButton update_id chat_id message users = do
 answerUser :: Integer -> Integer -> String -> InfoUsers -> Req a
 answerUser update_id chat_id message users = do
   let newRepeat = countRepeat chat_id (fst users)
-  sendTelegram ["/sendMessage?chat_id=", show chat_id, "&text=", message] newRepeat
+  let args = B.pack <$> ["/sendMessage?chat_id=", show chat_id, "&text=", message]
+  sendTelegram args newRepeat
   loop (update_id + 1) users
 
 sendHelp :: Integer -> Integer -> InfoUsers -> Req a
 sendHelp update_id chat_id users = do
-  sendTelegram ["/sendMessage?chat_id=", show chat_id, "&text=", messageHelp] defaultRepeat
+  let args = B.pack <$> ["/sendMessage?chat_id=", show chat_id, "&text=", messageHelp]
+  sendTelegram args 1
   loop (update_id + 1) users
       
 
 loop :: Integer -> InfoUsers -> Req a
 loop count users = do
-  js <- receiveTelegram ["/getUpdates?offset=", show count]
+  js <- receiveTelegram (B.pack <$> ["/getUpdates?offset=", show count])
   
   let resParse = fromResultToList $ parse parseTelegram (responseBody js)
-  --liftIO $ print $ resParse
+  liftIO $ print $ resParse
   when (null resParse) (loop count users)
   
   let (message, update_id, chat_id) = head resParse
